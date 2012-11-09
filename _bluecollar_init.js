a42c1eb22ece8d1c5103cda5978fda8f6c780f61
Neal Watkins <nealwatkins@gmail.com>
2012-11-08 17:21:53 -0800

    eighth commit

--- _bluecollar_init.js
+++ _bluecollar_init.js
@@ -4,8 +4,8 @@ app.rq = app.rq || []; //ensure array is defined. rq = resource queue.
 
 
 
-//app.rq.push(['extension',0,'convertSessionToOrder','extensions/checkout_passive/extension.js']);
-app.rq.push(['extension',0,'convertSessionToOrder','extensions/checkout_nice/extension.js']);
+app.rq.push(['extension',0,'convertSessionToOrder','extensions/checkout_passive/extension.js']);
+//app.rq.push(['extension',0,'convertSessionToOrder','extensions/checkout_nice/extension.js']);
 app.rq.push(['extension',0,'store_checkout','extensions/store_checkout.js']);
 app.rq.push(['extension',0,'store_prodlist','extensions/store_prodlist.js']);
 app.rq.push(['extension',0,'store_navcats','extensions/store_navcats.js']);
@@ -26,12 +26,26 @@ app.rq.push(['extension',1,'analytics_google','extensions/analytics_google.js','
 app.rq.push(['templateFunction','productTemplate','onCompletes',function(P) {
 	$( ".tabbedProductContent",$('#productTemplate_'+app.u.makeSafeHTMLId(P.pid))).tabs();
 	}]);
+	
+app.rq.push(['templateFunction','homepageTemplate','onCompletes',function(P) {
+	$( "#mainSlider").removeClass("displayNone");
+	}]);
+/* app.rq.push(['templateFunction','homepageTemplate','onLeave',function(P) {
+	$( "#mainSlider").addClass("displayNone");
+	}]); */
 
 app.rq.push(['script',0,(document.location.protocol == 'file:') ? app.vars.httpURL+'jquery/config.js' : app.vars.baseURL+'jquery/config.js']); //The config.js is dynamically generated.
 app.rq.push(['script',0,app.vars.baseURL+'model.js']); //'validator':function(){return (typeof zoovyModel == 'function') ? true : false;}}
 app.rq.push(['script',0,app.vars.baseURL+'includes.js']); //','validator':function(){return (typeof handlePogs == 'function') ? true : false;}})
 app.rq.push(['script',1,app.vars.baseURL+'jeditable.js']); //used for making text editable (customer address). non-essential. loaded late.
 app.rq.push(['script',0,app.vars.baseURL+'controller.js']);
+
+//cycle used for slideshow
+app.rq.push(['script',0,app.vars.baseURL+'cycle.js']);
+
+
+
+
 
 //sample of an onDeparts. executed any time a user leaves this page/template type.
 app.rq.push(['templateFunction','homepageTemplate','onDeparts',function(P) {app.u.dump("just left the homepage")}]);
